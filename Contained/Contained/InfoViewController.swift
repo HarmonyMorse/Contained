0aa71c815c4ff083625756a5d47dafa8dd92f0eb
HarmonyMorse <harmonymorse@icloud.com>
2022-07-09 17:33:09 -0700

    Finished "Build the Info Sequence" except for "Edit the two titles on the second and third info controllers to remove the "Title" text."

--- Contained/Contained/InfoViewController.swift
+++ Contained/Contained/InfoViewController.swift
@@ -8,7 +8,12 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
+    @IBAction func done(_ sender: Any) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
