dddd38a248941c59d2bf7e6cce3421cdaa78f4f3
HarmonyMorse <harmonymorse@icloud.com>
2022-07-09 17:49:42 -0700

    "Build the Setting Screen" done

--- Contained/Contained/SettingsViewController.swift
+++ Contained/Contained/SettingsViewController.swift
@@ -8,7 +8,15 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-
+    var this = true
+    @IBAction func toggleRoll(_ sender: UISwitch) {
+        Settings.shared.shouldRoll = sender.isOn
+    }
+    @IBAction func toggleZoom(_ sender: UISwitch) {
+        Settings.shared.shouldZoom = sender.isOn
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
